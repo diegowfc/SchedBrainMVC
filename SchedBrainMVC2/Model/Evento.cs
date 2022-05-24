e5fb4aadd659682362bb260e94241a40d62fb44f
Diego <diegofernas@gmail.com>
2022-05-24 20:45:53 -0300

    Criação da model

--- SchedBrainMVC2/Model/Evento.cs
+++ SchedBrainMVC2/Model/Evento.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,19 @@ namespace SchedBrainMVC2.Model
 {
     public class Evento
     {
+        [Required]
+        public int ID { get; set; }
+        
+        public string NomeEvento { get; set; }
+        public string LocalEvento { get; set; }
+        public string DescricaoEvento { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime DataInicio { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime DataTermino { get; set; }
+        public string Periodicidade { get; set; }
+        public string Status { get; set; }
+        public string Foto { get; set; }
 
     }
 }
