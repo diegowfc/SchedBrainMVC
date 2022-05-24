15534b15283672672b2e80ea08083ef57c03a049
Diego <diegofernas@gmail.com>
2022-05-24 20:27:32 -0300

    Inicio

---
+++ SchedBrainMVC2/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchedBrainMVC2.Data
+{
+    public class DataContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost\\SQLEXPRESS; Initial Catalog=BDSchedBrain; Integrated Security=SSPI");
+        }
+    }
+}
