a15c2ac1a9bde4edd6cb0d572f9e2f67bcc1c919
Diego <diegofernas@gmail.com>
2022-05-24 21:11:20 -0300

    Validação dos dados e criação do Evento View

---
+++ SchedBrainMVC2/View/EventoView.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace SchedBrainMVC2.View
+{
+    partial class EventoView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.rdoCancelado = new System.Windows.Forms.RadioButton();
+            this.lstContatos = new System.Windows.Forms.ListBox();
+            this.txtPesquisar = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.dtpDataTermino = new System.Windows.Forms.DateTimePicker();
+            this.dtpDataInicio = new System.Windows.Forms.DateTimePicker();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.pcbAnexo = new System.Windows.Forms.PictureBox();
+            this.rdoAndamento = new System.Windows.Forms.RadioButton();
+            this.rdoConcluido = new System.Windows.Forms.RadioButton();
+            this.cboPeriodicidade = new System.Windows.Forms.ComboBox();
+            this.lblDataTermino = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLocal = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pcbAnexo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rdoCancelado
+            // 
+            this.rdoCancelado.AutoSize = true;
+            this.rdoCancelado.Location = new System.Drawing.Point(202, 473);
+            this.rdoCancelado.Name = "rdoCancelado";
+            this.rdoCancelado.Size = new System.Drawing.Size(81, 19);
+            this.rdoCancelado.TabIndex = 56;
+            this.rdoCancelado.TabStop = true;
+            this.rdoCancelado.Text = "Cancelado";
+            this.rdoCancelado.UseVisualStyleBackColor = true;
+            this.rdoCancelado.Visible = false;
+            // 
+            // lstContatos
+            // 
+            this.lstContatos.FormattingEnabled = true;
+            this.lstContatos.ItemHeight = 15;
+            this.lstContatos.Location = new System.Drawing.Point(2, 358);
+            this.lstContatos.Name = "lstContatos";
+            this.lstContatos.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.lstContatos.Size = new System.Drawing.Size(279, 109);
+            this.lstContatos.TabIndex = 41;
+            // 
+            // txtPesquisar
+            // 
+            this.txtPesquisar.Location = new System.Drawing.Point(309, 8);
+            this.txtPesquisar.Name = "txtPesquisar";
+            this.txtPesquisar.PlaceholderText = "Pesquise por Nome, Local, Datas, Status e Contatos";
+            this.txtPesquisar.Size = new System.Drawing.Size(303, 23);
+            this.txtPesquisar.TabIndex = 55;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(712, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(314, 13);
+            this.label2.TabIndex = 54;
+            this.label2.Text = "Para executar uma ação, clique com o botão direito sob o evento";
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Location = new System.Drawing.Point(2, 186);
+            this.txtDescricao.Multiline = true;
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.PlaceholderText = "Descrição do evento";
+            this.txtDescricao.Size = new System.Drawing.Size(281, 49);
+            this.txtDescricao.TabIndex = 43;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(309, 38);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(714, 483);
+            this.flowLayoutPanel1.TabIndex = 53;
+            // 
+            // dtpDataTermino
+            // 
+            this.dtpDataTermino.Location = new System.Drawing.Point(2, 300);
+            this.dtpDataTermino.Name = "dtpDataTermino";
+            this.dtpDataTermino.Size = new System.Drawing.Size(281, 23);
+            this.dtpDataTermino.TabIndex = 45;
+            // 
+            // dtpDataInicio
+            // 
+            this.dtpDataInicio.Location = new System.Drawing.Point(2, 256);
+            this.dtpDataInicio.Name = "dtpDataInicio";
+            this.dtpDataInicio.Size = new System.Drawing.Size(281, 23);
+            this.dtpDataInicio.TabIndex = 44;
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Location = new System.Drawing.Point(208, 498);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCadastrar.TabIndex = 48;
+            this.btnCadastrar.Text = "Salvar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // pcbAnexo
+            // 
+            this.pcbAnexo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pcbAnexo.Image = global::SchedBrainMVC2.Properties.Resources.Foto;
+            this.pcbAnexo.Location = new System.Drawing.Point(10, 9);
+            this.pcbAnexo.Name = "pcbAnexo";
+            this.pcbAnexo.Size = new System.Drawing.Size(271, 113);
+            this.pcbAnexo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcbAnexo.TabIndex = 52;
+            this.pcbAnexo.TabStop = false;
+            // 
+            // rdoAndamento
+            // 
+            this.rdoAndamento.AutoSize = true;
+            this.rdoAndamento.Location = new System.Drawing.Point(88, 473);
+            this.rdoAndamento.Name = "rdoAndamento";
+            this.rdoAndamento.Size = new System.Drawing.Size(106, 19);
+            this.rdoAndamento.TabIndex = 49;
+            this.rdoAndamento.TabStop = true;
+            this.rdoAndamento.Text = "Em andamento";
+            this.rdoAndamento.UseVisualStyleBackColor = true;
+            // 
+            // rdoConcluido
+            // 
+            this.rdoConcluido.AutoSize = true;
+            this.rdoConcluido.Location = new System.Drawing.Point(2, 473);
+            this.rdoConcluido.Name = "rdoConcluido";
+            this.rdoConcluido.Size = new System.Drawing.Size(80, 19);
+            this.rdoConcluido.TabIndex = 47;
+            this.rdoConcluido.TabStop = true;
+            this.rdoConcluido.Text = "Concluído";
+            this.rdoConcluido.UseVisualStyleBackColor = true;
+            // 
+            // cboPeriodicidade
+            // 
+            this.cboPeriodicidade.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboPeriodicidade.Items.AddRange(new object[] {
+            "<<PERIODICIDADE>>",
+            "TODOS OS DIAS",
+            "A CADA 2 SEMANAS",
+            "A CADA MÊS",
+            "A CADA ANO"});
+            this.cboPeriodicidade.Location = new System.Drawing.Point(2, 329);
+            this.cboPeriodicidade.Name = "cboPeriodicidade";
+            this.cboPeriodicidade.Size = new System.Drawing.Size(281, 23);
+            this.cboPeriodicidade.TabIndex = 46;
+            this.cboPeriodicidade.Tag = "1";
+            // 
+            // lblDataTermino
+            // 
+            this.lblDataTermino.AutoSize = true;
+            this.lblDataTermino.Location = new System.Drawing.Point(2, 282);
+            this.lblDataTermino.Name = "lblDataTermino";
+            this.lblDataTermino.Size = new System.Drawing.Size(92, 15);
+            this.lblDataTermino.TabIndex = 51;
+            this.lblDataTermino.Text = "Data de término";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(2, 238);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 15);
+            this.label1.TabIndex = 50;
+            this.label1.Text = "Data de início";
+            // 
+            // txtLocal
+            // 
+            this.txtLocal.Location = new System.Drawing.Point(2, 157);
+            this.txtLocal.Name = "txtLocal";
+            this.txtLocal.PlaceholderText = "Local do evento";
+            this.txtLocal.Size = new System.Drawing.Size(281, 23);
+            this.txtLocal.TabIndex = 42;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(2, 128);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.PlaceholderText = "Nome do evento";
+            this.txtNome.Size = new System.Drawing.Size(281, 23);
+            this.txtNome.TabIndex = 40;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.NeverBlink;
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // EventoView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1035, 534);
+            this.Controls.Add(this.rdoCancelado);
+            this.Controls.Add(this.lstContatos);
+            this.Controls.Add(this.txtPesquisar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDescricao);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.dtpDataTermino);
+            this.Controls.Add(this.dtpDataInicio);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.pcbAnexo);
+            this.Controls.Add(this.rdoAndamento);
+            this.Controls.Add(this.rdoConcluido);
+            this.Controls.Add(this.cboPeriodicidade);
+            this.Controls.Add(this.lblDataTermino);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLocal);
+            this.Controls.Add(this.txtNome);
+            this.Name = "EventoView";
+            this.Text = "EventoView";
+            this.Load += new System.EventHandler(this.EventoView_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pcbAnexo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private RadioButton rdoCancelado;
+        private ListBox lstContatos;
+        private TextBox txtPesquisar;
+        private Label label2;
+        private TextBox txtDescricao;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private DateTimePicker dtpDataTermino;
+        private DateTimePicker dtpDataInicio;
+        private Button btnCadastrar;
+        private PictureBox pcbAnexo;
+        private RadioButton rdoAndamento;
+        private RadioButton rdoConcluido;
+        private ComboBox cboPeriodicidade;
+        private Label lblDataTermino;
+        private Label label1;
+        private TextBox txtLocal;
+        private TextBox txtNome;
+        private ErrorProvider errorProvider1;
+    }
+}
---
+++ SchedBrainMVC2/View/EventoView.cs
@@ -0,0 +1,163 @@
+﻿using SchedBrainMVC2.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SchedBrainMVC2.View
+{
+    public partial class EventoView : Form
+    {
+        public EventoView()
+        {
+            InitializeComponent();
+        }
+
+        private void EventoView_Load(object sender, EventArgs e)
+        {
+            cboPeriodicidade.SelectedIndex = 0;
+            txtDescricao.ScrollBars = ScrollBars.Vertical;
+            txtNome.Focus();
+        }
+
+        bool imagemAlterada = false;
+
+        /// <summary>
+        /// Limpa os campos de entrada após a inserção de um novo evento.
+        /// </summary>
+        public void limpaCampo()
+        {
+            imagemAlterada = false;
+            //pcbAnexo.Image = global::SchedBrain.Properties.Resources.Foto;
+            txtLocal.Text = string.Empty;
+            txtNome.Text = string.Empty;
+            txtDescricao.Text = string.Empty;
+            cboPeriodicidade.SelectedIndex = 0;
+            dtpDataInicio.Value = DateTime.Now;
+            dtpDataTermino.Value = DateTime.Now;
+            rdoAndamento.Checked = false;
+            rdoConcluido.Checked = false;
+            txtNome.Focus();
+        }
+
+        public void editaCampo(string _nomeEvento, string _localEvento, string descricao, DateTime inicio, DateTime termino, string periodicidade, string status, string foto)
+        {
+
+            //eventoAlvo = eventos[_nomeEvento].Nome;
+            imagemAlterada = true;
+            rdoCancelado.Visible = true;
+            pcbAnexo.Tag = foto.ToString();
+
+            try
+            {
+                if (foto != "")
+                    pcbAnexo.Image = new Bitmap(foto);
+            }
+            catch (Exception)
+            {
+                pcbAnexo.Image = Properties.Resources.Foto;
+                pcbAnexo.Image.Tag = "";
+            }
+
+            txtNome.Text = _nomeEvento;
+            txtLocal.Text = _localEvento;
+            txtDescricao.Text = descricao;
+            dtpDataInicio.Value = inicio;
+            dtpDataTermino.Value = termino;
+            cboPeriodicidade.Text = periodicidade.ToString();
+            if (status == "Concluido")
+                rdoConcluido.Checked = true;
+            else if (status == "Em Andamento")
+                rdoAndamento.Checked = true;
+            else
+                rdoCancelado.Checked = true;
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            errorProvider1.Clear();
+            bool erros = false, itemRepetido = false;
+            string status, contatos = "";
+            rdoCancelado.Visible = false;
+
+
+            pcbAnexo.WaitOnLoad = imagemAlterada;
+
+            if (txtNome.Text.Trim() == "")
+            {
+                errorProvider1.SetError(txtNome, "Campo deve ser preenchido!");
+                erros = true;
+            }
+
+            if (txtLocal.Text.Trim() == "")
+            {
+                errorProvider1.SetError(txtLocal, "Campo deve ser preenchido!");
+                erros = true;
+            }
+
+            if (txtDescricao.Text.Trim() == "")
+            {
+                errorProvider1.SetError(txtDescricao, "Campo deve ser preenchido!");
+                erros = true;
+            }
+
+            if (dtpDataInicio.Value.Date > dtpDataTermino.Value.Date)
+            {
+                errorProvider1.SetError(dtpDataInicio, "Data início não pode ser maior que Data término!");
+                erros = true;
+            }
+
+            if (rdoAndamento.Checked == false && rdoConcluido.Checked == false && rdoCancelado.Checked == false)
+            {
+                errorProvider1.SetError(rdoAndamento, "Campo deve ser preenchido!");
+                erros = true;
+            }
+
+            if (cboPeriodicidade.SelectedIndex == 0)
+            {
+                errorProvider1.SetError(cboPeriodicidade, "Campo deve ser preenchido!");
+                erros = true;
+            }
+
+
+            if (imagemAlterada == false)
+            {
+                errorProvider1.SetError(pcbAnexo, "Escolha uma foto!");
+                erros = true;
+            }
+
+            if (rdoAndamento.Checked)
+                status = "Em Andamento";
+            else if (rdoConcluido.Checked)
+                status = "Concluido";
+            else
+                status = "Cancelado";
+
+            if (erros == false)
+            {
+
+                foreach (string lista in lstContatos.SelectedItems)
+                {
+                    contatos += lista.ToString() + "; ";
+                }
+
+                //Evento evento = new Evento(txtNome.Text, txtLocal.Text, txtDescricao.Text, dtpDataInicio.Value, dtpDataTermino.Value, cboPeriodicidade.Text, status, contatos, pcbAnexo.Tag.ToString());
+
+                if (itemRepetido == false)
+                {
+                    DialogResult dr = MessageBox.Show("Evento salvo com sucesso!", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    if (dr == DialogResult.OK)
+                        limpaCampo();
+                }
+            }
+        }
+
+       
+    }
+}
